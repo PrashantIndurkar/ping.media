24471afbcdcf26b72ca00517d9d3e6094c6074ca
prashantindurkar <prashantri2000@gmail.com>
2024-06-03 18:28:17 +0530

    feat: auth completed

---
+++ src/app/CustomProvider.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { SessionProvider } from "next-auth/react";
+
+// This wraps the whole app in a session provider to get the session
+// client side components can't have
+// import { getServerSession } from "next-auth"; so we need to wrap the whole app in a session provider using next-auth/react
+
+export default function CustomProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,16 +1,49 @@
 import prisma from "@/DB/db.config";
-import { AuthOptions } from "next-auth";
+import { AuthOptions, ISODateString } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
+export type CustomSession = {
+  user?: CustomUser;
+  expires?: ISODateString;
+};
+
+export type CustomUser = {
+  id: string | null;
+  name?: string | null;
+  username?: string | null;
+  email?: string | null;
+};
+
 export const authOptions: AuthOptions = {
+  // custom pages
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    async session(params) {
+      const { session, token } = params;
+      session.user = token.user as CustomUser;
+
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
       name: "Credentials",
-
+      // Credentials: It enables users to sign in using email and password credentials directly within your application.
+      // only for sign in or login or sign up
       credentials: {
         email: {},
         password: { label: "Password", type: "password" },
       },
+      // authorize: validates the provided credentials and returns user data if authentication is successful.
       async authorize(credentials, req) {
         const user = await prisma.user.findUnique({
           where: {
