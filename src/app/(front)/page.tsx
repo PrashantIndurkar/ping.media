38550e3b7fcf90c97e1867e03c9c411c765dd6f0
prashantindurkar <prashantri2000@gmail.com>
2024-06-06 15:33:31 +0530

    feat: notification completed

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -9,14 +9,14 @@ import Logo from "@/components/common/Logo";
 export default function Home() {
   return (
     <>
-      <header className="h-14 border-b sticky top-0 left-0 right-0 px-4 dark:bg-zinc-900 z-10 flex items-center justify-betweens w-full ">
+      <header className="h-14 border-b sticky top-0 left-0 right-0 px-4 dark:bg-gray-900/15 z-10 flex items-center justify-betweens w-full ">
         <>
           <MobileSidebar />
           <Logo />
         </>
         <h3 className="text-lg font-medium ml-auto md:ml-0">Scroll</h3>
       </header>
-      <div className="sticky top-0 justify-center w-full border-b bg-zinc-50 dark:bg-zinc-900 py-4 flex items-center z-20 ">
+      <div className="sticky top-0 justify-center w-full border-b bg-zinc-50 dark:bg-gray-900/15 py-4 flex items-center z-20 ">
         <div className="relative mx-10 w-full max-w-full">
           <Avatar className="absolute left-3 top-1/2 transform -translate-y-1/2 h-8 w-8">
             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
