e587f6baad5837f78ff804fe13a8ab3f04b17e48
prashantindurkar <prashantri2000@gmail.com>
2024-06-10 13:49:56 +0530

    feat: build log out and bug fix, protect home page

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,13 +1,15 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { IoAdd } from "react-icons/io5";
 import Post from "@/components/common/Post";
 import MobileSidebar from "@/components/common/MobileSidebar";
 import Logo from "@/components/common/Logo";
 import CreatePost from "@/components/CreatePost";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/options";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <>
+      {/* {session && (JSON.stringify(session) as string)} */}
       <header className="h-14 border-b sticky top-0 left-0 right-0 px-4 dark:bg-gray-900/15  flex items-center justify-betweens w-full ">
         <>
           <MobileSidebar />
---
+++ src/components/common/AlertDialogLogout.tsx
@@ -0,0 +1,51 @@
+"use client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
+
+export function AlertDialogLogout() {
+  const logout = () => {
+    signOut({ callbackUrl: "/login", redirect: true });
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          className="dark:text-zinc-400 text-zinc-500 dark:hover:text-zinc-500 transition duration-150 ease-in-out text-xs"
+        >
+          Log Out
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to Log out?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Logging out will end your session. You will need to log in again to
+            use the app.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-red-500 hover:bg-red-600 text-white"
+            onClick={logout}
+          >
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
